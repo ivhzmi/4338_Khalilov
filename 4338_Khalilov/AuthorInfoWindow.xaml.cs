7e2eec776228bac9822be35e3498b3d816281c21
Ислам Халилов <ivhmzi@gmail.com>
2026-02-07 16:27:59 +0300

    Добавлено новое окно: AuthorInfoWindow

---
+++ 4338_Khalilov/AuthorInfoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4338_Khalilov
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorInfoWindow.xaml
+    /// </summary>
+    public partial class AuthorInfoWindow : Window
+    {
+        public AuthorInfoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
